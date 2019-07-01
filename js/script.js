c61d760d7f5e37a56ecb830889a6ef7b39a17093
Yapishyna <epishinat@gmail.com>
2019-07-01 12:28:23 +0300

    подключила скрипты и кату

---
+++ js/script.js
@@ -0,0 +1,6 @@
+
+var block=document.querySelector(".title-btn");
+var popup=document.querySelector(".hide-form");
+block.addEventListener("click",function (evt) {
+popup.classList.toggle("visual");
+});
