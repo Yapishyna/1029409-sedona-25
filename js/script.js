d6794e58314e6b528b242d32ae6c58c68e93bec0
Yapishyna <epishinat@gmail.com>
2019-07-02 15:21:56 +0300

    защита 3 попытка

--- js/script.js
+++ js/script.js
@@ -1,6 +1,28 @@
-
 var block=document.querySelector(".title-btn");
 var popup=document.querySelector(".hide-form");
-block.addEventListener("click",function (evt) {
-popup.classList.toggle("visual");
+var form=popup.querySelector(".hotel-search");
+var arrival=popup.querySelector("[name=arrival-date]");
+var departure=popup.querySelector("[name=departure-date]");
+var adult=popup.querySelector("[name=adult]");
+var children=popup.querySelector("[name=children]");
+var storage=localStorage.getItem("adult");
+var number=localStorage.getItem("children");
+
+block.addEventListener("click", function (evt) {
+ popup.classList.toggle("visual");
+ arrival.focus();
+ if (storage && number) {
+   adult.value=storage;
+   children.value=number;
+ }
+});
+
+form.addEventListener("submit", function (evt) {
+  if (!arrival.value || !departure.value || !adult.value || !children.value) {
+    evt.preventDefault();
+    console.log("Необходимо ввести данные");
+  } else {
+    localStorage.setItem("adult", adult.value);
+    localStorage.setItem("children",children.value);
+  }
 });
